444a24978dfc0d2295544d3344c7d4f52f39ac33
sckunkle <ppdplayer@gmail.com>
2020-01-06 15:48:54 -0800

    Make Proper Driving(?)

--- startup/src/main/java/frc/robot/Robot.java
+++ startup/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
@@ -17,19 +19,29 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
+  private SpeedController zero, one, two, three;
+  private SpeedControllerGroup left, right;
   private Joystick stick;
+  private DifferentialDrive MotorDrive;
   //private Joystick m_rightStick;
 
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    zero = new PWMVictorSPX(0);
+    one = new PWMVictorSPX(1);
+    two = new PWMVictorSPX(2);
+    three = new PWMVictorSPX(3);
+
+    left = new SpeedControllerGroup(zero, one);
+    right = new SpeedControllerGroup(two, three);
+
+    MotorDrive = new DifferentialDrive(left, right);
     stick = new Joystick(0);
     //m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.arcadeDrive(stick.getY(), stick.getX());
+    MotorDrive.arcadeDrive(-stick.getY(), -stick.getX());
   }
 }
