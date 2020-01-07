22eee96a31e268b232d96dba06c38fe3bc986743
powpingdone <ppdplayer@gmail.com>
2020-01-06 18:43:06 -0600

    FIX BS

--- startup/src/main/java/frc/robot/Auto/AutoSegment.java
+++ startup/src/main/java/frc/robot/Auto/AutoSegment.java
@@ -1,14 +1,31 @@
-public class MoveOffLine {
+package frc.robot.auto;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Timer;
+
+public class AutoSegment {
     private DifferentialDrive motorDrive;
+    private Timer timer;
 
     //---------------------------------------------------------------\\
     //--Constructor--\\
-    public MoveOffLine(DifferentialDrive motorDrive) {
+    public AutoSegment(DifferentialDrive motorDrive) {
         this.motorDrive = motorDrive;
+
+        timer = new Timer();
     }
 
-    public shootPowerCells() {
+    //---------------------------------------------------------------\\
+    //--Scores points via shooting balls into the power port--\\
+    public void shootPowerCells() {
         
     }
 
+    //---------------------------------------------------------------\\
+    //--Moves the robot off of the starting line--\\
+    public void moveOffLine() {
+        motorDrive.arcadeDrive(.5, 0);
+        timer.delay(1);
+        motorDrive.arcadeDrive(0,0);
+    }
 }
