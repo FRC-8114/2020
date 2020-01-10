862cc1ccd7c5c79020b2bacfe8191d40f4aa8d4b
Isaac <iasueltenfuss@gvtc.com>
2020-01-10 14:17:35 -0600

    Fix robot adjusting values in autonomous

--- startup/src/main/java/frc/robot/Auto/AutoSegment.java
+++ startup/src/main/java/frc/robot/Auto/AutoSegment.java
@@ -23,16 +23,16 @@ public class AutoSegment {
     //---------------------------------------------------------------\\
     //--Moves the robot off of the starting line--\\
     public void moveOffLine(Timer timer) {
-        if(timer.get()<1) {
-            motorDrive.tankDrive(.2, .2);
-        } else if(timer.get()<2) {
-            motorDrive.tankDrive(.4, .4);
-        } else if(timer.get()<3) {
-            motorDrive.tankDrive(.8, .8);
+        if(timer.get()<2) {
+            motorDrive.tankDrive(.2, .2, false);
         } else if(timer.get()<4) {
-            motorDrive.tankDrive(.4, .4);
-        } else if(timer.get()<5) {
-            motorDrive.tankDrive(.2, .2);
+            motorDrive.tankDrive(.4, .4, false);
+        } else if(timer.get()<8) {
+            motorDrive.tankDrive(.8, .8, false);
+        } else if(timer.get()<10) {
+            motorDrive.tankDrive(.4, .4, false);
+        } else if(timer.get()<12) {
+            motorDrive.tankDrive(.2, .2, false);
         } else {
             motorDrive.stopMotor();
         }
