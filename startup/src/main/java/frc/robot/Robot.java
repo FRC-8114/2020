25955ee5611efe34f4bc1c68b8838d11686df550
Jack <jackgholt@gmail.com>
2020-01-14 17:01:39 -0600

    Prepared for branch merge

--- startup/src/main/java/frc/robot/Robot.java
+++ startup/src/main/java/frc/robot/Robot.java
@@ -97,25 +97,7 @@ public class Robot extends TimedRobot {
     //-Buttons
     controller = new XboxController(0); //joystick ID zero, can change
     //-Camera
-    new Thread(() -> {
-      UsbCamera mainCamera = CameraServer.getInstance().startAutomaticCapture();
-      mainCamera.setResolution(320, 240);
-      CvSink cvSink = CameraServer.getInstance().getVideo();
-      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 320, 240);
 
-      Mat source = new Mat();
-      Mat filtered = new Mat();
-      Mat output = new Mat();
-
-      while(!Thread.interrupted()) {
-        if (cvSink.grabFrame(source) == 0) {
-          continue;
-        }
-       //Core.inRange(source, new Scalar(0, 0, 0), new Scalar(0, 255, 0), filtered);
-        //Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-        outputStream.putFrame(source);
-      }
-    }).start();
     /**
      * PCODE for button init
      * HashMap<Button, String> buttons = new HashMap<Button, String>(9);
