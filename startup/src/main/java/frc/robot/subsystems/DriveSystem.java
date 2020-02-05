c3d1cfa5a61583e512e0af5267188ebba295a24d
Jack <doesnotcomputeeeee@gmail.com>
2020-02-04 18:21:17 -0600

    Changed to CAN

--- startup/src/main/java/frc/robot/subsystems/DriveSystem.java
+++ startup/src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -9,37 +9,42 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import com.ctre.phoenix.CANifier;
 
 import edu.wpi.first.wpilibj.XboxController;
 
 public class DriveSystem extends SubsystemBase {
   private XboxController controller;
   
-  private PWMVictorSPX backLeft, frontLeft, backRight, frontRight;
+  private WPI_VictorSPX backLeft, frontLeft, backRight, frontRight;
   private SpeedControllerGroup left, right;
   private DifferentialDrive driveTrain;
 
   public DriveSystem(XboxController controller) {
     this.controller = controller;
 
-    backLeft = new PWMVictorSPX(0);
-    frontLeft = new PWMVictorSPX(1);
-    backRight = new PWMVictorSPX(2);
-    frontRight = new PWMVictorSPX(3);
+    backLeft = new WPI_VictorSPX(0);
+    frontLeft = new WPI_VictorSPX(1);
+    //backRight = new PWMVictorSPX(2);
+    //frontRight = new PWMVictorSPX(3);
 
-    left = new SpeedControllerGroup(backLeft, frontLeft);
-    right = new SpeedControllerGroup(backRight, frontRight);
+    //left = new SpeedControllerGroup(backLeft, frontLeft);
+    //right = new SpeedControllerGroup(backRight, frontRight);
 
-    driveTrain = new DifferentialDrive(left, right);
+    //driveTrain = new DifferentialDrive(left, right);
   }
 
   @Override
   public void periodic() {
-    driveTrain.tankDrive(-Math.floor(controller.getY(Hand.kLeft)*100)/100, -Math.floor(controller.getY(Hand.kRight)*100)/100, false);
+    //driveTrain.tankDrive(-Math.floor(controller.getY(Hand.kLeft)*100)/100, -Math.floor(controller.getY(Hand.kRight)*100)/100, false);
+    backLeft.set(-Math.floor(controller.getY(Hand.kLeft)*100)/100);
+    frontLeft.set(-Math.floor(controller.getY(Hand.kRight)*100)/100);
+    backLeft.getDeviceID();
+    //backLeft.setLEDOutput(0.75, LEDChannel LEDChannelA);
   }
 
   public void drive(double left, double right) {
