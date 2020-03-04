3c6ca305a851eda703003e115864510a8bcaa1eb
Jack <doesnotcomputeeeee@gmail.com>
2020-03-03 18:19:24 -0600

    Implemented DriveBackward Command

--- startup/src/main/java/frc/robot/commands/DriveBackward.java
+++ startup/src/main/java/frc/robot/commands/DriveBackward.java
@@ -5,10 +5,33 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class DriveBackward extends CommandBase {
     private DriveTrain driveTrain;
-    private double distance;
+    private final double distance;
 
     public DriveBackward(DriveTrain driveTrain, double distance) {
         this.driveTrain = driveTrain;
         this.distance = distance;
+        driveTrain.resetEncoders();
+
+        addRequirements(driveTrain);
+    }
+
+    public void execute() {
+        if(distance*.25<1) {
+            if(driveTrain.getRightEncoderDistance() > -distance*.25) {
+                driveTrain.driveBackward(-(driveTrain.getRightEncoderDistance())/(distance*.25)*.8);
+            } else if (driveTrain.getRightEncoderDistance() > -distance*.75) {
+                driveTrain.driveBackward(-.8);
+            } else {
+                driveTrain.driveBackward(-(driveTrain.getRightEncoderDistance()-(distance*.75))/(distance-(distance*.75))*.8);
+            }
+        } else {
+            if(driveTrain.getRightEncoderDistance() > -1) {
+                driveTrain.driveBackward(-driveTrain.getRightEncoderDistance()*.8);
+            } else if (driveTrain.getRightEncoderDistance() > -distance-1) {
+                driveTrain.driveBackward(-.8);
+            } else {
+                driveTrain.driveBackward(-(driveTrain.getRightEncoderDistance()-(driveTrain.getRightEncoderDistance()-1))*.8);
+            }
+        }
     }
 }
