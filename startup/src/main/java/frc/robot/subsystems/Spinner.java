7367f7fc3d2c2efd51c22dd1eaa5808dc5e36049
Alg3bra1c <iasueltenfuss@gvtc.com>
2020-03-06 17:38:59 -0600

    Updated Encoders

--- startup/src/main/java/frc/robot/subsystems/Spinner.java
+++ startup/src/main/java/frc/robot/subsystems/Spinner.java
@@ -7,15 +7,25 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class Spinner extends SubsystemBase {  
   private WPI_VictorSPX spinner;
+  private Encoder spin_e;
 
   public Spinner() {
     spinner = new WPI_VictorSPX(21);
+    spin_e = new Encoder(2,3,false,EncodingType.k1X);
+    spin_e.setDistancePerPulse(wheelCircumference(20));
+    spin_e.setSamplesToAverage(5);
+  }
+
+  public void resetSpinEncoder() {
+    spin_e.reset();
   }
 
   public void runSpinner(double speed) {
@@ -24,5 +34,17 @@ public class Spinner extends SubsystemBase {
 
   public void reverseSpinner(double speed) {
     spinner.set(-speed);
+  }
+
+  public double inchesToMeters(double inches) {
+    return inches / 39.37;
+  }
+
+  public double wheelCircumference(double diameter) {
+    return Math.PI * inchesToMeters(diameter);
+  }
+
+  public double getEncoderDistance() {
+    return spin_e.getDistance();
   }
 }
